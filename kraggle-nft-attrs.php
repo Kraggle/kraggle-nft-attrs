9bc97a7cbd47693ed1790364ed89aad9a0decb90
Kraggle <kraggle@live.co.uk>
2022-01-24 17:04:39 +0000

    v 1.1.1

--- kraggle-nft-attrs.php
+++ kraggle-nft-attrs.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Kraggles NFT Attributes
  * Plugin URI:        http://kragglesites.com
  * Description:       Shortcode for showing off nft attributes
- * Version:           1.1.0
+ * Version:           1.1.1
  * Author:            Kraggle
  * Author URI:        http://kragglesites.com/
  * License:           GPLv3
@@ -25,7 +25,7 @@
  * Domain Path:       /
  */
 
-
+define('KNA_VERSION', '1.1.1');
 
 // If this file is called directly, abort.
 if (!defined('WPINC')) {
@@ -36,11 +36,9 @@ define('KNA_PLUGIN_BASENAME', plugin_basename(__FILE__));
 define('KNA_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('KNA_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-function kna_shortcode() {
-	$version = '1.1.0';
-
-	wp_enqueue_style('kna', KNA_PLUGIN_URL . 'style/style.css', [], $version);
-	wp_enqueue_script('module-kna', KNA_PLUGIN_URL . 'js/script.js', ['jquery'], $version);
+add_shortcode('nft-attrs', function() {
+	wp_enqueue_style('kna', KNA_PLUGIN_URL . 'style/style.css', [], KNA_VERSION);
+	wp_enqueue_script('module-kna', KNA_PLUGIN_URL . 'js/script.js', ['jquery'], KNA_VERSION);
 
 	$fa = 'https://images.kgl.app';
 	$json = json_decode(file_get_contents("$fa/martians/attributes"), true);
@@ -128,8 +126,76 @@ function kna_shortcode() {
 	ob_end_clean();
 
 	return $html;
-}
-add_shortcode('nft-attrs', 'kna_shortcode');
+});
+
+global $used_variations, $layers;
+
+add_shortcode('nft-rarity', function($attrs) {
+	global $used_variations, $layers;
+	
+	extract(shortcode_atts(
+		array(
+			'variations' => false,
+			'columns' => 3
+		),
+		$attrs
+	));
+
+	if (!$variations) return '';
+	$variations = json_decode(file_get_contents(get_attached_file($variations)));
+	
+	$fa = 'https://images.kgl.app';
+	$layers = json_decode(file_get_contents("$fa/martians/attributes"));
+	
+	if (!isset($layers->directory)) return '';
+	$layers = $layers->directory;
+	
+	wp_enqueue_style('kna', KNA_PLUGIN_URL . 'style/style.css', [], KNA_VERSION);
+	wp_enqueue_script('module-kna', KNA_PLUGIN_URL . 'js/script.js', ['jquery'], KNA_VERSION);
+
+	// reset the variations, in case the shortcode is 
+	// used more than once on a page
+	$used_variations = (object) [];
+
+	prepare_variations($variations, 1); // Do the set layers
+	prepare_variations($variations, 2); // Do the matching layers
+	prepare_variations($variations, 3); // Get the rest
+	
+	ob_start(); ?>
+
+	<div class="kna-rarity-wrap" style="grid-template-columns: repeat(<?= $columns ?> , 1fr);">
+
+		<?php foreach ($variations as $variation) { ?>
+			
+			<div class="kna-variation">
+				<div class="kna-img-wrap">
+					<?php foreach ($variation->layers as $layer) { ?>
+						<img src="<?= $layer->url ?>" alt="layer">
+					<?php } ?>
+				</div>
+				<div class="kna-describe">
+					<span class="kna-title"><?= $variation->name ?></span>
+					<span class="kna-block">
+						<span class="kna-key">Layers</span>
+						<?php $all = [];
+						foreach ($variation->layers as $layer => $value)
+							$all[] = $layers->$layer->singular;
+						echo  implode(', ', $all); ?>
+					</span>
+					<span class="kna-block"><span class="kna-key">Rarity</span><?= $variation->rarity ?></span>
+					<span class="kna-block"><span class="kna-key">Quantity</span><?= $variation->quantity ?></span>
+				</div>
+			</div>
+		
+		<?php } ?>
+	
+	</div>
+
+<?php $html = ob_get_contents();
+	ob_end_clean();
+
+	return $html;
+});
 
 function kna_script_as_module($tag, $handle, $src) {
 	if (preg_match('/^module-/', $handle)) {
@@ -139,3 +205,119 @@ function kna_script_as_module($tag, $handle, $src) {
 	return $tag;
 }
 add_filter('script_loader_tag', 'kna_script_as_module', 10, 3);
+
+function prepare_variations(&$variations, $run) {
+	global $used_variations;
+	
+	foreach ($variations as &$variation) {
+		foreach ($variation->layers as $attr => &$type) {
+			if (!isset($used_variations->$attr)) 
+				$used_variations->$attr = [];
+				
+			if ($run == 1) {
+				// this run adds preset to used_variations
+				
+				$ts = explode(' ', $type);
+				$types = (object) [];
+				foreach ($ts as &$t) {
+					if (preg_match('/~/', $t)) {
+						$t = explode('~', $t);
+						$types->{$t[0]} = $t[1];
+					} else if (preg_match('/random|unique/', $t)) 
+						$types->$t = true;
+					else if (preg_match('/^https?:\/\//', $t)) 
+						$types = get_layer($attr, 'url', $t);
+					else 
+						$types = get_layer($attr, 'file', $t);
+				}
+				$type = $types;
+				
+			} else if ($run == 2) {
+				// this run gets any layers that are match
+				
+				foreach ($type as $key => $value) {
+					if ($key == 'match') {
+						$matches = get_layer([$attr, $value]);
+						$type = $matches[0];
+						$variation->layers->$value = $matches[1];
+					}
+				}
+			} else {
+				if (!isset($type->file))
+					$type = get_layer($attr);
+			}
+		}
+	}
+}
+
+function get_layer($attr, $type = false, $find = null) {
+	global $used_variations, $layers;
+	$layer = '';
+	
+	/* This grabs the layer when url or file is set */
+	if ($type && $find) {
+		$items_1 = $layers->$attr->items;
+		
+		foreach ($items_1 as $item) {
+			if ($item->$type == $find) {
+				$used_variations->$attr[] = $item->file;
+				return $item;
+			}
+		}
+		
+		return $layer;
+	}
+	
+	/* This picks up mathching attributes */
+	if (is_array($attr)) {
+		
+		$items_1 = $layers->{$attr[0]}->items;
+		$items_2 = $layers->{$attr[1]}->items;
+		$used_1 = $used_variations->{$attr[0]};
+		$used_2 = $used_variations->{$attr[1]};
+		shuffle($items_1);
+		
+		foreach ($items_1 as $item_1) {
+			$item_2 = false;
+			
+			if (isset($item_1->matches) && isset($item_1->matches->{$attr[1]})) {
+				foreach ($items_2 as $item) {
+					if ($item->file == $item_1->matches->{$attr[1]})
+						$item_2 = $item;
+				}
+			}
+			
+			if ($item_2 && !in_array($item_1->file, $used_1) && !in_array($item_2->file, $used_2)) {
+				$used_variations->{$attr[0]}[] = $item_1->file;
+				$used_variations->{$attr[1]}[] = $item_2->file;
+				return [$item_1, $item_2];
+			}
+		}
+		
+		return [(object) ['error' => true], (object) ['error' => true]];
+	}
+	
+	/* This finds a unique layer */
+	$items_1 = $layers->$attr->items;
+	$used = $used_variations->$attr;
+	do {
+		$layer = $items_1[array_rand($items_1)];
+	} while (in_array($layer->file, $used));
+	$used_variations->$attr[] = $layer->file;
+	
+	return $layer;
+}
+
+if (!function_exists('logger')) {
+	function logger() {
+		$db = array_shift(debug_backtrace());
+		$line = $db['line'];
+		$file = $db['file'];
+
+		$msg = "$file:$line [logger]";
+
+		foreach (func_get_args() as $arg) {
+			error_log($msg . (in_array(gettype($arg), ['string', 'double', 'integer']) ? $arg : json_encode($arg, JSON_PRETTY_PRINT)));
+		}
+	}
+}
